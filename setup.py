75f9b30526251eb4c49beb4ecd4d858fb502b6ca
Victor Bolinches <vicboma1@users.noreply.github.com>
2020-05-07 16:58:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(name='test',
       author='Geekshubs',
       packages=find_packages('src'),
       package_dir={'': 'src'},
-      py_modules=[splitext(basename(path))[0] for path in glob('src/es/geekshubs/python/*.py')],
+      py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
      )
